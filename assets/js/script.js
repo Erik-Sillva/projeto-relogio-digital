fa9f39ec4ff84f65a0efab2042e4925bd1a5422c
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-12-11 15:26:22 -0300

    incluindo data dinamica

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+const days = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
 const clock = setInterval(function time() {
     let dateToday = new Date();
+    let day = dateToday.getDate();
+    let mon = dateToday.getMonth() + 1;
+    let yea = dateToday.getFullYear();
     let hrs = dateToday.getHours();
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
@@ -12,6 +18,9 @@ const clock = setInterval(function time() {
     if (min < 10) min = '0' + min;
     if (sec < 10) sec = '0' + sec;
 
+    days.innerText = day + '/'
+    month.innerText = mon + '/'
+    year.innerText = yea
     hours.innerText = hrs
     minutes.innerText = min
     seconds.innerText = sec
