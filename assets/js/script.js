4e91f43b681e98fdd2d51e73c2225f5a9165f2c3
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-12-11 02:18:35 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+    let hrs = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hrs < 10) hrs = '0' + hrs;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+
+    hours.textContent = hrs
+    minutes.textContent = min
+    seconds.textContent = sec
+})
