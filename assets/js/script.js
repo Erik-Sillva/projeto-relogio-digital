d512acef746816c2d53f79293de65124415988ed
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-12-14 01:32:04 -0300

    Finalizando cronometro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-const days = document.getElementById('day');
-const month = document.getElementById('month');
-const year = document.getElementById('year');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+// ----------------RELÓGIO----------------//
 const clock = setInterval(function time() {
+    const days = document.getElementById('day');
+    const month = document.getElementById('month');
+    const year = document.getElementById('year');
+    const hours = document.getElementById('hours');
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
+
     let dateToday = new Date();
     let day = dateToday.getDate();
     let mon = dateToday.getMonth() + 1;
@@ -15,6 +17,7 @@ const clock = setInterval(function time() {
     if (hrs < 10) hrs = '0' + hrs;
     if (min < 10) min = '0' + min;
     if (sec < 10) sec = '0' + sec;
+
     days.innerText = day + '/'
     month.innerText = mon + '/'
     year.innerText = yea
@@ -22,3 +25,60 @@ const clock = setInterval(function time() {
     minutes.innerText = min
     seconds.innerText = sec
 })
+
+// ----------------CRONÔMETRO----------------//
+
+const hrsctxt = document.querySelector('#hours-c');
+const minctxt = document.querySelector('#minutes-c');
+const secctxt = document.querySelector('#seconds-c');
+const mlsctxt = document.querySelector('#milisegundos-c');
+let hrsc = 0
+let minc = 0
+let secc = 0
+let mlsc = 0
+let interval
+
+function start() {
+    interval = setInterval(function () {
+        mlsc++
+        if (mlsc==100) {
+            secc++
+            if (secc<10) {
+                secc = '0' + secc
+            }
+            mlsc=0
+        }else if (secc==60) {
+            minc++
+            if (minc<10) {
+                minc = '0' + minc
+            }
+            secc=0
+        } else if (minc==60) {
+            hrsc++
+            if (hrsc<10) {
+                hrsc = '0' + hrsc
+            }
+            minc=0
+        }
+        mlsctxt.innerText = mlsc
+        secctxt.innerText = secc
+        minctxt.innerText = minc
+        hrsctxt.innerText = hrsc
+    })
+}
+
+function pause() {
+    clearInterval(interval)
+}
+function reset() {
+    clearInterval(interval)
+    hrsc = 0
+    minc = 0
+    secc = 0
+    mlsc = 0
+
+    mlsctxt.innerText = mlsc
+    secctxt.innerText = secc
+    minctxt.innerText = minc
+    hrsctxt.innerText = hrsc
+}
